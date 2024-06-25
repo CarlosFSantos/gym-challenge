89fdfe4b708cd1adfca679a53bc9604551277658
Carlos Fogaça <carlosjfogaca@gmail.com>
2024-06-25 18:17:24 -0300

    manipulating instructor data

--- instructors.js
+++ instructors.js
@@ -1,5 +1,26 @@
 const fs = require('fs')
 const data = require("./data.json")
+// show //
+
+exports.show = function(req,res){
+    const {id} = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+    
+    if (!foundInstructor) return res.send('Instructor not found!!!')
+
+    const instructor = {
+        ...foundInstructor,
+        age: "",
+        services: foundInstructor.services.split(","),
+        created_at:""
+    }
+
+    return res.render("instructors/show", {instructor})    
+}
+
 // create //
 exports.post = function(req,res){
     const keys = Object.keys(req.body)
@@ -9,10 +30,23 @@ exports.post = function(req,res){
             return res.send("Please, fill all fields")
         }
     }
-    req.body.birth = Date.parse(req.body.birth)
-    req.body.created_at = Date.now()
+    let {avatar_url, name,services, gender, birth} = req.body
 
-    data.instructors.push(req.body)
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at,
+    })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if (err) return res.send("Write file error")
