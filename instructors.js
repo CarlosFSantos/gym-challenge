cd26be8aa58d77d25521e865019509aab2b36166
Carlos Fogaça <carlosjfogaca@gmail.com>
2024-06-28 17:49:28 -0300

    creating instructor editing page

--- instructors.js
+++ instructors.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const data = require("./data.json")
+const {age, date} = require("./data")
 // show //
 
 exports.show = function(req,res){
@@ -10,12 +11,12 @@ exports.show = function(req,res){
     })
     
     if (!foundInstructor) return res.send('Instructor not found!!!')
-
+   
     const instructor = {
         ...foundInstructor,
-        age: "",
+        age: age(foundInstructor.birth),
         services: foundInstructor.services.split(","),
-        created_at:""
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at)
     }
 
     return res.render("instructors/show", {instructor})    
@@ -53,4 +54,24 @@ exports.post = function(req,res){
         
         return res.redirect("/instructors")
     })
+}
+
+// edit
+
+exports.edit =  function(req,res){
+
+    const {id} = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+    
+    if (!foundInstructor) return res.send('Instructor not found!!!')
+
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+    return res.render('instructors/edit', {instructor})
 }
